d77f7fc09861088c9eabd2390b4fe3bb25c6324d
whatawholeone <whatawholeone@users.noreply.github.com>
2016-06-14 13:58:03 +0100

    Add files via upload

--- LedController/LedController.ino
+++ LedController/LedController.ino
@@ -4,6 +4,10 @@
 
 #define NUM_LEDS 50
 #define DATA_PIN 13
+
+#define SPEED_SAVE 1
+#define PATTERN_SAVE 10
+
 int RECV_PIN = 2;
 CRGB leds[NUM_LEDS];
 IRrecv irrecv(RECV_PIN);
@@ -11,42 +15,92 @@ decode_results results;
 int patternNum = 0;
 int patternDelay = 50;
 int speedStep = 5;
-bool ledsOn = true;
-
+int pattern = 1;
+bool ledsOn = false;
+uint8_t baseHue = 0;
 
 void setup() {
   Serial.begin(9600);
-  //while (!Serial) {  ;  }
-  attachInterrupt(2, CHECK_IR, CHANGE);
+  //while (!Serial) {   ;  }
   irrecv.enableIRIn(); // Start the receiver
   LEDS.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-  patternDelay = epRead();
+  patternDelay = epRead(SPEED_SAVE);
+  Serial.print("Speed from memory: ");
+  Serial.println(patternDelay);
+  patternNum = epRead(PATTERN_SAVE);
+  Serial.print("Pattern from memory: ");
+  Serial.println(patternNum);
 }
 
 void loop() {
+
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+
+    switch (results.value) {
+      case 0xFFE01F:
+        Serial.println("Button: Power");
+        powerSwitch();
+        break;
+      case 0xFFA05F:
+        Serial.println("Button: UP");
+        speedUp();
+        break;
+      case 0xFF20DF:
+        Serial.println("Button: DOWN");
+        slowDown();
+        break;
+      case 0xFFF00F:
+        Serial.println("Button: Rainbow");
+        pattern = 2;
+        epWrite(PATTERN_SAVE, pattern );
+        break;
+      case 0xFFC837:
+        Serial.println("Button: Cylon");
+        pattern = 1;
+        epWrite(PATTERN_SAVE, pattern );
+        break;
+    }
+    irrecv.resume(); // Receive the next value
+  }
+
   if (ledsOn) {
     playPattern();
+    EVERY_N_MILLISECONDS( 20 ) {
+      baseHue++;
+    }
   }
 }
 
 void playPattern() {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    fadeall();
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(patternDelay);
+
+  if (pattern = 1) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      fadeall();
+      leds[i] = CRGB::Red;
+      FastLED.show();
+      if (irrecv.decode(&results)) break;
+      delay(patternDelay);
+    }
+    for (int i = (NUM_LEDS) - 1; i >= 0; i--) {
+      fadeall();
+      leds[i] = CRGB::Red;
+      FastLED.show();
+      if (irrecv.decode(&results)) break;
+      delay(patternDelay);
+    }
   }
-  for (int i = (NUM_LEDS) - 1; i >= 0; i--) {
-    fadeall();
-    leds[i] = CRGB::Red;
+
+  if (pattern = 2) {
+    fill_rainbow( leds, NUM_LEDS, baseHue, 7);
     FastLED.show();
-    delay(patternDelay);
   }
 }
 
 void fadeall() {
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i].fadeToBlackBy(128);
+    if (irrecv.decode(&results)) break;
   }
 }
 
@@ -55,7 +109,7 @@ void speedUp() {
   if ( patternDelay > speedStep) {
     patternDelay = patternDelay - speedStep;
     Serial.println(patternDelay);
-    epWrite(patternDelay );
+    epWrite(SPEED_SAVE, patternDelay );
   }
 }
 
@@ -63,7 +117,7 @@ void slowDown() {
   // incease delay
   patternDelay = patternDelay + speedStep;
   Serial.println(patternDelay);
-  epWrite(patternDelay);
+  epWrite(SPEED_SAVE, patternDelay);
 }
 
 void powerSwitch() {
@@ -83,46 +137,12 @@ void powerSwitch() {
   }
 }
 
-void CHECK_IR() {
-  while (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-    irrecv.resume();
-  }
+void epWrite(int location, int data) {
+  EEPROM.put(location, data);
 }
 
-void readIR() {
-  while (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-
-    switch (results.value) {
-      case 0xFFE01F:
-        Serial.println("Button: Power");
-        powerSwitch();
-        break;
-      case 0xFFA05F:
-        Serial.println("Button: UP");
-        speedUp();
-        break;
-      case 0xFF20DF:
-        Serial.println("Button: DOWN");
-        slowDown();
-        break;
-    }
-    irrecv.resume(); // Receive the next value
-  }
-  //  delay(100);
-}
-
-void epWrite(int i) {
-  EEPROM.put(0, i);
-  Serial.print("save speed to memory: ");
-  Serial.println(i);
-}
-
-int epRead() {
+int epRead(int location) {
   int i = 0;
-  EEPROM.get(0, i);
-  Serial.print("read speed from memory: ");
-  Serial.println(i);
+  EEPROM.get(location, i);
   return (i);
 }
